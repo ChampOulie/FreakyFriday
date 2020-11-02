2122882e6ceb55bc0de954fb9da4defa0f7143bc
Oulie <70588001+ChampOulie@users.noreply.github.com>
2020-11-02 19:08:04 +0100

    Add files via upload

---
+++ docs/assets/scripts/navscroll.js
@@ -0,0 +1,13 @@
+/* navbar disappear on scroll down (https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp): */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
